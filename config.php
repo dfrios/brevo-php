102af61bf3bc8e2f8cb3814b3ccbbcc027c6d1bb
David Rios <david@davidrios.co>
2024-01-23 14:45:18 -0500

    fix: "config.php" file deleted
    "config.php" file must be out of repo

--- config.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-
-define('BREVO_URL','https://api.sendinblue.com/v3/');
-define('BREVO_APIKEY', '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');
-define('BREVO_LIST', 86); // List "Tienes cáncer, Feb 3 / 2024"
